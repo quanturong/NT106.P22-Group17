7b7f3b38b933e9658e07073fe8c6731541db6cab
quanturong <165626599+quanturong@users.noreply.github.com>
2025-07-04 05:13:45 +0700

    Fix display flasks

--- RRCards/Assets/Scripts/RoomManager.cs
+++ RRCards/Assets/Scripts/RoomManager.cs
@@ -18,11 +18,22 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     private Player localPlayer;
     private Player opponentPlayer;
+    private bool hasSpawned = false;
 
     void Start()
     {
         localPlayer = PhotonNetwork.LocalPlayer;
         SetupPlayerNames();
+
+        Invoke("CheckAndSpawn", 1f);
+    }
+
+    void CheckAndSpawn()
+    {
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2 && !hasSpawned)
+        {
+            SpawnFlasksForAllPlayers();
+        }
     }
 
     private void SetupPlayerNames()
@@ -46,8 +57,6 @@ public class RoomManager : MonoBehaviourPunCallbacks
                 string opponentName = opponentPlayer.NickName ?? "Opponent";
                 opponentPlayerNameText.text = opponentName;
             }
-            SpawnFlasksInFrame(leftSlots);
-            SpawnFlasksInFrame(rightSlots);
         }
         else
         {
@@ -61,6 +70,11 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         UpdateOpponentName();
+
+        if (PhotonNetwork.IsMasterClient && !hasSpawned)
+        {
+            Invoke("CheckAndSpawn", 1f);
+        }
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
@@ -69,35 +83,198 @@ public class RoomManager : MonoBehaviourPunCallbacks
         UpdateOpponentName();
     }
 
-    void SpawnFlasksInFrame(RectTransform[] slots)
+    void SpawnFlasksForAllPlayers()
     {
-        if (leftSlots.Length == 0 || rightSlots.Length == 0 || flaskPrefabs.Length == 0)
+        if (hasSpawned) return;
+
+        hasSpawned = true;
+
+        List<FlaskSpawnData> spawnData = GenerateSpawnData();
+
+        ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable();
+        props["FlaskData"] = JsonUtility.ToJson(new FlaskDataWrapper { flasks = spawnData.ToArray() });
+        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+
+        SpawnFromData(spawnData);
+    }
+
+    List<FlaskSpawnData> GenerateSpawnData()
+    {
+        List<FlaskSpawnData> data = new List<FlaskSpawnData>();
+
+        Random.InitState(System.DateTime.Now.Millisecond + PhotonNetwork.LocalPlayer.ActorNumber);
+
+        int maxSlots = Mathf.Min(leftSlots.Length, rightSlots.Length);
+
+        int flaskCountA = Random.Range(1, Mathf.Min(5, maxSlots + 1));
+
+        int flaskCountB = Random.Range(1, Mathf.Min(5, maxSlots + 1));
+
+        List<int> usedSlotsA = new List<int>();
+        for (int i = 0; i < flaskCountA; i++)
         {
-            Debug.LogError("Slots hoặc Prefabs chưa được gán hoặc tìm thấy.");
-            return;
+            int nextSlot = GetNextAvailableSlot(usedSlotsA, maxSlots);
+            if (nextSlot != -1)
+            {
+                usedSlotsA.Add(nextSlot);
+                data.Add(new FlaskSpawnData
+                {
+                    slotIndex = nextSlot,
+                    isPlayerA = true,
+                    flaskIndex = Random.Range(0, flaskPrefabs.Length)
+                });
+            }
         }
 
-        List<RectTransform> orderedSlots = new List<RectTransform>(slots);
-        orderedSlots.Sort((a, b) =>
+        List<int> usedSlotsB = new List<int>();
+        for (int i = 0; i < flaskCountB; i++)
         {
-            int indexA = a.GetComponent<SlotIndex>().index;
-            int indexB = b.GetComponent<SlotIndex>().index;
-            return indexA.CompareTo(indexB);
-        });
+            int nextSlot = GetNextAvailableSlot(usedSlotsB, maxSlots);
+            if (nextSlot != -1)
+            {
+                usedSlotsB.Add(nextSlot);
+                data.Add(new FlaskSpawnData
+                {
+                    slotIndex = nextSlot,
+                    isPlayerA = false,
+                    flaskIndex = Random.Range(0, flaskPrefabs.Length)
+                });
+            }
+        }
 
-        int flaskCount = Random.Range(1, Mathf.Min(5, orderedSlots.Count + 1));
-        for (int i = 0; i < flaskCount; i++)
+        return data;
+    }
+
+    int GetNextAvailableSlot(List<int> usedSlots, int maxSlots)
+    {
+        for (int i = 0; i < maxSlots; i++)
         {
-            RectTransform targetSlot = orderedSlots[i];
-            int randomFlaskIndex = Random.Range(0, flaskPrefabs.Length);
-            GameObject selectedFlask = flaskPrefabs[randomFlaskIndex];
-            GameObject flaskInstance = Instantiate(selectedFlask, targetSlot);
-            RectTransform flaskRect = flaskInstance.GetComponent<RectTransform>();
-            flaskRect.anchoredPosition = Vector2.zero;
-            flaskRect.localScale = Vector3.one;
-            Animator anim = flaskInstance.GetComponent<Animator>();
-            if (anim != null)
-                anim.Play("FlaskPopIn");
+            if (!usedSlots.Contains(i))
+            {
+                return i;
+            }
+        }
+        return -1; 
+    }
+
+    void ShuffleList<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
+
+    void SpawnFromData(List<FlaskSpawnData> spawnData)
+    {
+        ClearAllFlasks();
+
+        bool isPlayerA = PhotonNetwork.LocalPlayer.ActorNumber == 1;
+
+        foreach (var data in spawnData)
+        {
+            RectTransform[] targetSlots;
+
+            if (data.isPlayerA)
+            {
+                if (isPlayerA)
+                {
+                    targetSlots = leftSlots;
+                }
+                else
+                {
+                    targetSlots = rightSlots;
+                }
+            }
+            else
+            {
+                if (isPlayerA)
+                {
+                    targetSlots = rightSlots;
+                }
+                else
+                {
+                    targetSlots = leftSlots;
+                }
+            }
+
+            if (data.slotIndex < targetSlots.Length)
+            {
+                SpawnFlaskAtSlot(targetSlots[data.slotIndex], data.flaskIndex);
+            }
+        }
+    }
+
+    void SpawnFlaskAtSlot(RectTransform targetSlot, int flaskIndex)
+    {
+        if (flaskIndex >= flaskPrefabs.Length) return;
+
+        GameObject selectedFlask = flaskPrefabs[flaskIndex];
+        GameObject flaskInstance = Instantiate(selectedFlask, targetSlot);
+
+        RectTransform flaskRect = flaskInstance.GetComponent<RectTransform>();
+        flaskRect.anchoredPosition = Vector2.zero;
+        flaskRect.localScale = Vector3.one;
+
+        Animator anim = flaskInstance.GetComponent<Animator>();
+        if (anim != null)
+            anim.Play("FlaskPopIn");
+    }
+
+    void ClearAllFlasks()
+    {
+        foreach (var slot in leftSlots)
+        {
+            for (int i = slot.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(slot.GetChild(i).gameObject);
+            }
+        }
+
+        foreach (var slot in rightSlots)
+        {
+            for (int i = slot.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(slot.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        if (propertiesThatChanged.ContainsKey("FlaskData") && !hasSpawned)
+        {
+            hasSpawned = true;
+            string jsonData = (string)propertiesThatChanged["FlaskData"];
+            FlaskDataWrapper wrapper = JsonUtility.FromJson<FlaskDataWrapper>(jsonData);
+            SpawnFromData(wrapper.flasks.ToList());
+        }
+    }
+
+    [ContextMenu("Force Spawn Flasks")]
+    public void ForceSpawnFlasks()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            hasSpawned = false;
+            SpawnFlasksForAllPlayers();
+        }
+    }
+}
+
+[System.Serializable]
+public class FlaskSpawnData
+{
+    public int slotIndex;
+    public bool isPlayerA; 
+    public int flaskIndex;
+}
+
+[System.Serializable]
+public class FlaskDataWrapper
+{
+    public FlaskSpawnData[] flasks;
 }
